bbd8c0c84c1689b85e0fb42f0d91c17a975db6d2
Smiling-Hacker01 <vishal_239077@saitm.ac.in>
2025-10-20 11:39:09 +0530

    code is to find the majority element Its the element that appears more than ⌊n / 2⌋ times in an array.

---
+++ MajorityElement.java
@@ -0,0 +1,22 @@
+
+import java.util.HashMap;
+
+//code is to find the majority element Its the element that appears more than ⌊n / 2⌋ times in an array.
+public class MajorityElement {
+    public static int majorityElement(int[] nums){
+        int maxCount = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+
+             if(map.get(nums[i]) > nums.length/2) maxCount = nums[i];
+        }
+        return maxCount;
+    }
+    //main function
+    public static void main(String[] args) {
+        int[]nums = {2,2,1,1,1,2,2};
+        int result = majorityElement(nums);
+        System.out.println("The majority element in an array is " + result);
+    }
+}

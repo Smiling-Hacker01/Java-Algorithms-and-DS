504fc9850d3c039178f050b9f878827f7f1b1c7e
Smiling-Hacker01 <vishal_239077@saitm.ac.in>
2025-10-28 17:25:19 +0530

    code is to find the path in which a rat can travel in a maze using backtracking

--- RatInAMaze.java
+++ RatInAMaze.java
@@ -38,7 +38,6 @@ public class RatInAMaze {
     }
         return paths;
     }
-    
     //main function
     public static void main(String[] args) {
         int[][]maze = {{1, 0, 0, 0}, {1, 1, 0, 1}, {1, 1, 0, 0}, {0, 1, 1, 1}};

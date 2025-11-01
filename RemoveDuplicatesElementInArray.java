5951a9533ae80ef8e767fdd9292cfcf83654550f
Smiling-Hacker01 <vishal_239077@saitm.ac.in>
2025-11-01 17:06:22 +0530

    code is to remove duplicates from an unsorted array

---
+++ RemoveDuplicatesElementInArray.java
@@ -0,0 +1,19 @@
+//code is to remove duplicates in a array without sorting and return the modified array as a ArrayList
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class RemoveDuplicatesElementInArray {
+    public static ArrayList<Integer> removeDuplicates(int[]nums){
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        return new ArrayList<>(set);
+    }
+    //main function
+    public static void main(String[] args) {
+        int[]nums = {2,2,3,3,7,5};
+        ArrayList<Integer> result = removeDuplicates(nums);
+        System.out.println(result);
+    }
+}

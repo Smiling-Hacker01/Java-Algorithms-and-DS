400ef0ed6e4657a3cb3134e896f7a8c28385099f
Smiling-Hacker01 <vishal_239077@saitm.ac.in>
2025-11-05 17:10:37 +0530

    code is to rotate array left in a optimized way

--- RotateArrayLeftOptimized.java
+++ RotateArrayLeftOptimized.java
@@ -24,7 +24,7 @@ public class RotateArrayLeftOptimized {
     }
     //main function
     public static void main(String[] args) {
-        int[]nums = {1,2,3,4,5,6,7};
+        int[]nums = {1,2,3,4,5,6,7,8};
         int k = 3;
         rotate(nums, k);
         for (int i : nums) {

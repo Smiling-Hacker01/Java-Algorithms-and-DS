08aad94539ede925ce0cbb9c9703f83588cfa7ef
Smiling-Hacker01 <vishal_239077@saitm.ac.in>
2025-11-21 22:56:42 +0530

    code is to remove duplicates from an array usning hashset

--- RemoveDuplicatesElementInArray.java
+++ RemoveDuplicatesElementInArray.java
@@ -12,7 +12,7 @@ public class RemoveDuplicatesElementInArray {
     }
     //main function
     public static void main(String[] args) {
-        int[]nums = {2,2,3,3,7,5};
+        int[]nums = {2,2,3,3,6,5};
         ArrayList<Integer> result = removeDuplicates(nums);
         System.out.println(result);
     }

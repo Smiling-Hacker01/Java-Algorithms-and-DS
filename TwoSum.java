192b2baefb8c9a5c52b0a973e8e3c5bf599bcb39
Smiling-Hacker01 <vishal_239077@saitm.ac.in>
2026-02-25 23:03:26 +0530

    Added the validation check in Two sum sollution

--- TwoSum.java
+++ TwoSum.java
@@ -17,6 +17,10 @@ public static void main(String[] args) {
     int[] nums = {2, 7, 11, 15};
     int target = 9;
     int[] result = twoSum(nums, target);
-    System.out.println("Indices: " + result[0] + ", " + result[1]);
+    if (result.length == 2) {
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    } else {
+        System.out.println("No two sum solution found.");
+    }
 }
 }

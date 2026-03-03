42108f62097e96bf6f9fabffb5f7c492a5739834
Smiling-Hacker01 <vishal_239077@saitm.ac.in>
2026-03-03 22:59:40 +0530

    Code to find the maximum points

--- MaximumPoints.java
+++ MaximumPoints.java
@@ -23,7 +23,7 @@ public class MaximumPoints{
     }
     //main function
     public static void main(String[] args){
-        int[]nums = {6,2,3,4,7,2,1,7,1};
+        int[]nums = {6,2,3,5,7,2,1,7,1};
         int k = 4;
         int result = maxPoint(nums, k);
         System.out.println(result);
